4cf296e7ced1ce6901d73397f6479009bc4ffea1
SkyWing <82465366+SkyWing3@users.noreply.github.com>
2024-05-26 09:32:58 -0400

    Add files via upload

--- ENNI-Radio-by-SkyWing.lua
+++ ENNI-Radio-by-SkyWing.lua
@@ -3,7 +3,7 @@ require 'lib.moonloader'
 
 script_name("ENNI")
 script_url("https://github.com/SkyWing3/SAMP")
-script_version("25.05.2024")
+script_version("v1.0")
 local encoding = require 'encoding'
 encoding.default = 'CP1252'
 u8 = encoding.UTF8
@@ -16,14 +16,104 @@ local personas_autorizadas = {
     "ryan_wind",
 }
 
--- Se actualizo mi cleo jajaja
-
 -- https://github.com/qrlk/moonloader-script-updater
 local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
 local autoupdate_loaded = false
 local Update = nil
+local updated = false
 if enable_autoupdate then
-    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Actualizaci?n detectada. Estoy intentando actualizar desde '..thisScript().version..' a la '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Cargado %d desde %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('La descarga de la actualizacion esta completa.')sampAddChatMessage(b..'La actualizacion esta completa!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'La actualizacion no tuvo exito. Estoy ejecutando una version desactualizada...',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': No se requiere actualizaci?n.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': No puedo comprobar la actualizacion. Resignate o compruebalo ti mismo '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, Salga esperando la verificaci?n de actualizaci?n. Res?gnate o compru?balo t? mismo '..c)end end}]])
+    local updater_loaded, Updater = pcall(loadstring, [[return {
+        check = function(json_url, prefix, url)
+            local dlstatus = require('moonloader').download_status
+            local json = os.tmpname()
+            local started = os.clock()
+            if doesFileExist(json) then
+                os.remove(json)
+            end
+            downloadUrlToFile(json_url, json,
+                function(id, status, p1, p2)
+                    if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+                        if doesFileExist(json) then
+                            local f = io.open(json, 'r')
+                            if f then
+                                local info = decodeJson(f:read('*a'))
+                                updatelink = info.updateurl
+                                updateversion = info.latest
+                                f:close()
+                                os.remove(json)
+                                if updateversion ~= thisScript().version then
+                                    lua_thread.create(function(prefix)
+                                        local dlstatus = require('moonloader').download_status
+                                        local color = -1
+                                        sampAddChatMessage((prefix .. 'Actualizacion detectada. Estoy intentando actualizar de ' .. thisScript().version .. ' a ' .. updateversion), color)
+                                        wait(250)
+                                        downloadUrlToFile(updatelink, thisScript().path,
+                                            function(id3, status1, p13, p23)
+                                                if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                                                    print(string.format('Cargado %d desde %d.', p13, p23))
+                                                elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                                                    print('La descarga de la actualizacion esta completa.')
+                                                    sampAddChatMessage((prefix .. 'La actualizacion esta completa!'), color)
+                                                    goupdatestatus = true
+                                                    updated = true
+                                                    lua_thread.create(function()
+                                                        wait(500)
+                                                        thisScript():reload()
+                                                    end)
+                                                end
+                                                if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                                                    if goupdatestatus == nil then
+                                                        sampAddChatMessage((prefix .. 'La actualizacion no tuvo exito. Estoy ejecutando una version desactualizada...'), color)
+                                                        update = false
+                                                    end
+                                                end
+                                            end
+                                        )
+                                    end, prefix
+                                    )
+                                else
+                                    update = false
+                                    updated = true
+                                    sampAddChatMessage((prefix .. 'No se requiere actualizacion.'), -1)
+                                    print('v' .. thisScript().version .. ': No se requiere actualizacion.')
+                                    if info.telemetry then
+                                        local ffi = require "ffi"
+                                        ffi.cdef "int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"
+                                        local serial = ffi.new("unsigned long[1]", 0)
+                                        ffi.C.GetVolumeInformationA(nil, nil, 0, serial, nil, nil, nil, 0)
+                                        serial = serial[0]
+                                        local _, myid = sampGetPlayerIdByCharHandle(PLAYER_PED)
+                                        local nickname = sampGetPlayerNickname(myid)
+                                        local telemetry_url = info.telemetry ..
+                                            "?id=" ..
+                                            serial ..
+                                            "&n=" ..
+                                            nickname ..
+                                            "&i=" ..
+                                            sampGetCurrentServerAddress() ..
+                                            "&v=" .. getMoonloaderVersion() .. "&sv=" .. thisScript().version .. "&uptime=" .. tostring(os.clock())
+                                        lua_thread.create(function(url)
+                                            wait(250)
+                                            downloadUrlToFile(url)
+                                        end, telemetry_url)
+                                    end
+                                end
+                            end
+                        else
+                            print('v' .. thisScript().version .. ': No puedo comprobar la actualizacion. Resignate o compruebalo tu mismo en ' .. url)
+                            update = false
+                        end
+                    end
+                end
+            )
+            while update ~= false and os.clock() - started < 10 do
+                wait(100)
+            end
+            if os.clock() - started >= 10 then
+                print('v' .. thisScript().version .. ': timeout, se tardo demaciado en verificar la actualizacion. Resignate o compruebalo tu mismo en ' .. url)
+            end
+        end
+    }]])
     if updater_loaded then
         autoupdate_loaded, Update = pcall(Updater)
         if autoupdate_loaded then
@@ -37,12 +127,15 @@ end
 function main()
     if not isSampfuncsLoaded() or not isSampLoaded() then return end
     while not isSampAvailable() do wait(100) end
-    sampRegisterChatCommand("irc", sendIRCMessage)
-    sampRegisterChatCommand("ref", sendIRCMessage)
-    sampRegisterChatCommand("/c", conectar)
     if autoupdate_loaded and enable_autoupdate and Update then
         pcall(Update.check, Update.json_url, Update.prefix, Update.url)
     end
+    if not updated then
+        sampAddChatMessage("No funciona el actualizar", -1)
+    end
+    sampRegisterChatCommand("irc", sendIRCMessage)
+    sampRegisterChatCommand("ref", sendIRCMessage)
+    sampRegisterChatCommand("/c", conectar)
     while true do
         wait(1000)
         if connected then
@@ -52,8 +145,18 @@ function main()
 end
 
 function conectar()
-    local result, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
+    local _, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
     name = sampGetPlayerNickname(id)
+    local res = false
+    for i,k in ipairs(personas_autorizadas) do
+        if k == name:lower() then
+            res = true
+        end
+    end
+    if not res then
+        sampAddChatMessage(string.format('%s{FFFFFF}, no te encuentras en la base de datos para poder acceder a la radio.', name), 0xFF00CCFF)
+        return
+    end
     IRC = irc.new{nick = name}
     IRC:connect("irc.esper.net")
     IRC:join("#sampfenixzone")
